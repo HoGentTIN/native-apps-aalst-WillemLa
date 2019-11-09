21c6e1e7d9dc96fdf598b861e36cbb98506f0b1a
Willem Lapage <wille@DESKTOP-9287UCL>
2019-11-09 14:22:41 +0100

    CocktailApiService Afwerken

--- app/src/main/java/com/example/mycocktails/network/CocktailApiService.kt
+++ app/src/main/java/com/example/mycocktails/network/CocktailApiService.kt
@@ -1,2 +1,34 @@
 package com.example.mycocktails.network
 
+import com.example.mycocktails.domain.Cocktail
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+
+private const val BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface CocktailApiService {
+
+    @GET("filter.php?c=Ordinary_Drink")
+    suspend fun getCocktails(): List<Cocktail>
+}
+
+object CocktaiApi {
+    val retrofitService: CocktailApiService by lazy {
+        retrofit.create(CocktailApiService::class.java)
+    }
+}
