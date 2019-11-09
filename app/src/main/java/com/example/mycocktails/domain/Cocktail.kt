a94a8301a348754b35cde05066d5464b7138c09c
Willem Lapage <wille@DESKTOP-9287UCL>
2019-11-09 14:19:35 +0100

    Entity aanpassen voor API

--- app/src/main/java/com/example/mycocktails/domain/Cocktail.kt
+++ app/src/main/java/com/example/mycocktails/domain/Cocktail.kt
@@ -3,6 +3,7 @@ package com.example.mycocktails.domain
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.squareup.moshi.Json
 
 //voordeel data class: hash/toString/...
 @Entity(tableName = "personal_cocktail_table")
@@ -10,9 +11,11 @@ data class Cocktail(
 
 
     @ColumnInfo(name = "cocktailName")
+    @Json(name= "strDrink")
     var name: String = "",
 
     @ColumnInfo(name = "cocktailCategory")
+    @Json(name= "strCategory")
     var category: String = "",
 
     @PrimaryKey(autoGenerate = true)
